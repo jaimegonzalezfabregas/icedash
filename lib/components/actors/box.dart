7a059e42609692fc257ff4489a2d01a742e25256
Jaime <jaimegonzalezfabregas@gmail.com>
2025-11-10 23:32:00 +0100

    music pipeline

--- lib/components/actors/box.dart
+++ lib/components/actors/box.dart
@@ -1,5 +1,6 @@
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
+import 'package:flame_audio/flame_audio.dart';
 import 'package:icedash/components/actor.dart';
 import 'package:icedash/components/room.dart';
 import 'package:icedash/src/rust/api/main.dart';
@@ -8,10 +9,12 @@ class Box extends Actor {
   double timePerStep = 0.07;
 
   RoomComponent room;
-  Box(this.room, {super.position}) : super("hit_box.png");
+  Box(this.room, {super.position}) : super("box.png");
 
   @override
   Future<bool> hit(Direction dir) async {
+    FlameAudio.play('hit_box.mp3');
+
     var delta = Vector2.array(dir.dartVector());
     if (!await room.canBoxWalkInto(position + delta, dir)) {
       await room.hit(position + delta, dir, box: true);
