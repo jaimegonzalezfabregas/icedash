c81290631bf0402b16eb164929b8a8c57c4f5343
Jaime <jaimegonzalezfabregas@gmail.com>
2025-11-23 21:51:29 +0100

    perfonmance

--- lib/components/actors/box.dart
+++ lib/components/actors/box.dart
@@ -6,17 +6,53 @@ import 'package:icedash/components/room.dart';
 import 'package:icedash/extensions.dart';
 import 'package:icedash/src/rust/api/direction.dart';
 
+class BoxDisplay extends SpriteComponent {
+  @override
+  onLoad() async {
+    super.sprite = await Sprite.load("box.png");
+    super.size = Vector2.all(1);
+    super.bleed = 0.01;
+  }
+}
+
 class Box extends Actor {
   double secPerStep = 0.07;
 
   RoomComponent room;
+  late BoxDisplay boxDisplay;
   Box(this.room, {super.position}) : super("box.png");
+
+  @override
+  void onLoad() {
+    boxDisplay = BoxDisplay();
+    add(boxDisplay);
+  }
 
   @override
   Future<bool> hit(Direction dir) async {
     FlameAudio.play('hit_box.mp3');
 
-    return push(dir);
+    Vector2 destination = await predictHit(dir);
+    int movementLenght = (destination - position).length.floor();
+
+    if (movementLenght != 0) {
+      boxDisplay.position = position-destination;
+
+      position = destination;
+
+      boxDisplay.add(
+        MoveToEffect(
+          Vector2.all(0),
+          LinearEffectController(secPerStep * movementLenght),
+          onComplete: () {
+            room.hit(destination + dir.dartVector(), dir, box: true);
+          },
+        ),
+      );
+
+      return true;
+    }
+    return false;
   }
 
   Future<Vector2> predictHit(Direction dir) async {
@@ -28,27 +64,6 @@ class Box extends Actor {
     }
 
     return cursor;
-  }
-
-  Future<bool> push(Direction dir) async {
-    Vector2 destination = await predictHit(dir);
-    int movementLenght = (destination - position).length.floor();
-
-    print("moving box from $position to $destination, in $movementLenght ");
-
-    super.colision = false;
-    add(
-      MoveToEffect(
-        destination,
-        LinearEffectController(secPerStep * movementLenght),
-        onComplete: () {
-          super.colision = true;
-          room.hit(destination + dir.dartVector(), dir, box: true);
-        },
-      ),
-    );
-
-    return true;
   }
 
   @override
--- lib/components/player.dart
+++ lib/components/player.dart
@@ -14,7 +14,9 @@ class Player extends SpriteComponent with HasGameReference<IceDashGame> {
   Player({super.position}) : super(priority: 20, size: Vector2.all(1), anchor: Anchor.center);
 
   double secPerStep = 0.07;
-  bool sliding = false;
+
+  List<Direction> movementQueue = [];
+
   int? remainingMoves;
   int? remainingMovesReset;
 
@@ -52,18 +54,23 @@ class Player extends SpriteComponent with HasGameReference<IceDashGame> {
     );
   }
 
+  bool moving = false;
+
   void reset() {
-    if (!sliding) {
-      game.idWorld.reset();
-      position = game.idWorld.resetPlayerPos();
-      push(game.idWorld.getResetDirection());
-      remainingMoves = remainingMovesReset;
+    if (moving) {
+      return;
     }
+    movementQueue = [];
+    game.idWorld.reset();
+    position = game.idWorld.resetPlayerPos();
+    push(game.idWorld.getResetDirection());
+    remainingMoves = remainingMovesReset;
   }
 
-  Future<Vector2> predictHit(Direction dir, bool firstPush) async {
+  Future<Vector2> predictHit(Direction dir) async {
     Vector2 cursor = position;
     Vector2 delta = dir.dartVector();
+    bool firstPush = true;
 
     while ((await game.idWorld.canMove(cursor, cursor + delta, dir, firstPush))) {
       firstPush = false;
@@ -136,61 +143,63 @@ class Player extends SpriteComponent with HasGameReference<IceDashGame> {
     );
   }
 
-  Future<void> onHit(Direction dir, int movementLenght) async {
-    sliding = false;
-
-    Tile hitTile = await game.idWorld.getTile(position + dir.dartVector());
-
-    bool consecuences = await game.idWorld.hit(position + dir.dartVector(), dir);
-
-    int moveI = remainingMoves == null ? 1 : remainingMoves!;
-
-    String audio = 'move_${min(moveI, 17)}.mp3';
-
-    if (movementLenght != 0 || consecuences) {
-      if (remainingMoves != null) {
-        remainingMoves = remainingMoves! - 1;
-        print("remaining moves: $remainingMoves");
-        if (remainingMoves == 0) {
-          if (hitTile is! Tile_Outside) {
-            audio = "too_many_moves.mp3";
-            reset();
-          }
-        }
-      }
+  Future<void> movementDispatch() async {
+    if (movementQueue.isEmpty) {
+      return;
     }
 
-    if (hitTile is! Tile_Gate) {
-      FlameAudio.play(audio);
-    } else {
-      if (hitTile.field0 is GateMetadata_EntryOnly) {
-        FlameAudio.play(audio);
-      }
-    }
-  }
-
-  bool moving = false;
-
-  void push(Direction dir, {bool firstPush = true}) async {
     if (moving) {
       return;
     }
+
     moving = true;
 
-    Vector2 destination = await predictHit(dir, firstPush);
+    Direction dir = movementQueue.first;
+    movementQueue.removeAt(0);
+
+    Vector2 destination = await predictHit(dir);
 
     int movementLenght = (destination - position).length.floor();
 
     MoveToEffect effect = MoveToEffect(
       destination,
       LinearEffectController(movementLenght * secPerStep),
-      onComplete: () {
-        onHit(dir, movementLenght);
+      onComplete: () async {
         moving = false;
+
+        Tile hitTile = await game.idWorld.getTile(position + dir.dartVector());
+
+        bool consecuences = await game.idWorld.hit(position + dir.dartVector(), dir);
+        bool exiting = hitTile is Tile_Outside;
+
+        int moveI = remainingMoves == null ? 1 : remainingMoves!;
+
+        String audio = 'move_${min(moveI, 17)}.mp3';
+
+        if ((movementLenght != 0 || consecuences) && !exiting) {
+          if (remainingMoves != null) {
+            remainingMoves = remainingMoves! - 1;
+            print("remaining moves: $remainingMoves, exiting $exiting, $hitTile");
+            if (remainingMoves == 0) {
+              if (hitTile is! Tile_Outside) {
+                audio = "too_many_moves.mp3";
+                reset();
+              }
+            }
+          }
+
+          FlameAudio.play(audio);
+        }
+        movementDispatch();
       },
     );
 
     add(effect);
+  }
+
+  void push(Direction dir) async {
+    movementQueue.add(dir);
+    movementDispatch();
   }
 
   void rescueIfOutside(Direction rescueDir) async {
@@ -199,7 +208,7 @@ class Player extends SpriteComponent with HasGameReference<IceDashGame> {
     }
 
     if ((await game.idWorld.getTile(position)) is Tile_Outside) {
-      push(rescueDir, firstPush: false);
+      push(rescueDir);
     }
   }
 }
