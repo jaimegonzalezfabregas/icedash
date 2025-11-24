90134460bcdd4fbecd81318cba27cbafa4d4d728
Jaime <jaimegonzalezfabregas@gmail.com>
2025-11-24 20:11:14 +0100

    different import of main

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -6,9 +6,10 @@ import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/services.dart';
 import 'package:icedash/config.dart';
 import 'package:icedash/extensions.dart';
-import 'package:icedash/main.dart';
 import 'package:icedash/src/rust/api/direction.dart';
 import 'package:icedash/src/rust/api/tile.dart';
+
+import '../main.dart';
 
 class Player extends SpriteComponent with HasGameReference<IceDashGame> {
   Player({super.position}) : super(priority: 20, size: Vector2.all(1), anchor: Anchor.center);
